35f8de806ef76d0528e15bc320bd41368989350b
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-12-16 21:20:31 +0700

    nav is nice

---
+++ src/components/navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row w-full p-2 bg-PrimaryNav text-white justify-between">
+      <div id="links" className="p-5">
+        <ol className="flex flex-row font-bold gap-2">
+          <li><a href="/prestasi">Prestasi</a></li>
+          <li><a href="/Pendaftaran">Pendaftaran</a></li>
+          <li><a href="/Galeri">Galeri</a></li>
+          <li><a href="/Event">Event</a></li>
+        </ol>
+      </div>
+      <Image
+        src="/assets/Logo-Strada-small-128.png"
+        alt="Logo Strada"
+        width="128"
+        height="128"
+        className=""
+      />
+    </nav>
+  );
+}
